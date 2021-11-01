cabfc65b429402d2d2784cab845995c5cb4a71a8
orindholt <83012141+orindholt@users.noreply.github.com>
2021-11-01 12:58:28 +0100

    Add first version of functionality

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const swipeElement = document.querySelectorAll(".swipe-text");
+
+let touchCordStart;
+let touchCordMove;
+
+
+swipeElement.forEach(element => {
+    let childrenWidth = element.previousElementSibling.offsetWidth;
+    element.addEventListener("touchstart", (e) =>{
+        touchCordStart = Math.floor(e.touches[0].clientX);
+        console.log(touchCordStart);
+    });
+    
+    element.addEventListener("touchmove", (e) => {
+        touchCordMove = Math.floor(e.touches[0].clientX);
+        //move 
+        if(touchCordMove < touchCordStart 
+        && touchCordMove > touchCordStart-childrenWidth){
+            e.target.style.transform = `translateX(${touchCordMove-touchCordStart}px)`;
+        }
+    });
+    
+    element.addEventListener("touchend", (e) => {
+        if(touchCordMove < touchCordStart-childrenWidth){
+            //reset to del
+            e.target.style.transform = `translateX(-${childrenWidth}px)`;
+        } else {
+            //reset to 0
+            e.target.style.transform = "translateX(0px)";
+        }
+    });
+
+    element.previousElementSibling.addEventListener("click", ()=>{
+        element.parentElement.style.display = "none";
+    });
+});
